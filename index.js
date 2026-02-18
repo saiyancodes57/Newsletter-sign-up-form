95505400978df103c9312bdc34a7baaed58e135a
saiyancodes57 <256424803+saiyancodes57@users.noreply.github.com>
2026-02-18 22:27:24 +0000

    Updated HTML and CSS

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const input = document.querySelector(".email");
-const labelCont = document.querySelector(".label-container");
-const errorLabel = document.createElement("p");
+// const input = document.querySelector(".email");
+// const labelCont = document.querySelector(".label-container");
+// const errorLabel = document.createElement("p");
 
-errorLabel.classList.add("text-preset-3", "error-text");
-errorLabel.textContent = "Valid email required";
+// errorLabel.classList.add("text-preset-3", "error-text");
+// errorLabel.textContent = "Valid email required";
 
-const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+// const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-const updateDom = (isValid) => {
-  if (isValid) {
-    errorLabel.remove();
-    input.classList.remove("error");
-  } else {
-    labelCont.appendChild(errorLabel);
-    input.classList.add("error");
-  }
-};
+// const updateDom = (isValid) => {
+//   if (isValid) {
+//     errorLabel.remove();
+//     input.classList.remove("error");
+//   } else {
+//     labelCont.appendChild(errorLabel);
+//     input.classList.add("error");
+//   }
+// };
 
-input.addEventListener("input", function (e) {
-  const value = e.target.value;
+// input.addEventListener("input", function (e) {
+//   const value = e.target.value;
 
-  if (value.length === 0) {
-    errorLabel.remove();
-    input.classList.remove("error");
-  } else {
-    updateDom(emailRegex.test(value));
-  }
-});
+//   if (value.length === 0) {
+//     errorLabel.remove();
+//     input.classList.remove("error");
+//   } else {
+//     updateDom(emailRegex.test(value));
+//   }
+// });
